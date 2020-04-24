e329fb282ff9c4383c31e646c34d8c1710fe468b
MD24 <matiasdaldi@gmail.com>
2020-04-24 18:29:47 -0400

    added on clicks, saving to local storage, and making sure text maintains even on reload

--- main.js
+++ main.js
@@ -1,6 +1,75 @@
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+var nineAm = "";
+var tenAm = "";
+var elevenAm = "";
+var twelvePm = "";
+var onePm = "";
+var twoPm = "";
+var threePm = "";
+var fourPm = "";
+var fivePm = "";
 
 setTextareaClass();
+
+maintainText();
+
+$(".firstHour").on("click", "button", function(){
+    nineAm = ($("#9").val().trim())
+    localStorage.setItem("9am", nineAm)
+    $("#9").text(nineAm)
+});
+
+$(".secondHour").on("click", "button", function(){
+    tenAm = ($("#10").val().trim())
+    localStorage.setItem("10am", tenAm)
+});
+
+$(".thirdHour").on("click", "button", function(){
+    elevenAm = ($("#11").val().trim())
+    localStorage.setItem("11am", elevenAm)
+});
+
+$(".fourthHour").on("click", "button", function(){
+    twelvePm = ($("#12").val().trim())
+    localStorage.setItem("12pm", twelvePm)
+});
+
+$(".fifthHour").on("click", "button", function(){
+    onePm = ($("#13").val().trim())
+    localStorage.setItem("1pm", onePm)
+});
+
+$(".sixthHour").on("click", "button", function(){
+    twoPm = ($("#14").val().trim())
+    localStorage.setItem("2pm", twoPm)
+});
+
+$(".seventhHour").on("click", "button", function(){
+    threePm = ($("#15").val().trim())
+    localStorage.setItem("3pm", threePm)
+});
+
+$(".eigthHour").on("click", "button", function(){
+    fourPm = ($("#16").val().trim())
+    localStorage.setItem("4pm", fourPm)
+});
+
+$(".ninthHour").on("click", "button", function(){
+    fivePm = ($("#17").val().trim())
+    localStorage.setItem("5pm", fivePm)
+});
+
+function maintainText() {
+    $("#9").text(localStorage.getItem("9am"));
+    $("#10").text(localStorage.getItem("10am"));
+    $("#11").text(localStorage.getItem("11am"));
+    $("#12").text(localStorage.getItem("12pm"));
+    $("#13").text(localStorage.getItem("1pm"));
+    $("#14").text(localStorage.getItem("2pm"));
+    $("#15").text(localStorage.getItem("3pm"));
+    $("#16").text(localStorage.getItem("4pm"));
+    $("#17").text(localStorage.getItem("5pm"));
+}
 
 function setTextareaClass() {
     var currentHour = moment().hour()
@@ -76,5 +145,4 @@ function setTextareaClass() {
         ninthHour.removeClass("present")
         ninthHour.addClass("future")
     }
-    console.log(moment().hour());
 };
